d40b1698371f0c0f7d720c6c431af86cc860728a
professor <professor@L2MP.IPIRANGA.FATEC>
2019-10-24 14:42:46 -0200

    Inclusao da biblioteca POI

---
+++ scel_v0/src/test/java/com/fatec/scel/gui/Constant.java
@@ -0,0 +1,15 @@
+package com.fatec.scel.gui;
+
+public class Constant {
+	 public static final String Path_TestData = "WebContent/WEB-INF/lib/";
+	  // public static final String Path_TestData2 = "E:/edson/2018edson/2018projetodidatico/20181sroteirodetesteguidd/";
+	   //C:\Users\esa\git\20181s_weby\ControleDeEmprestimo\WebContent\WEB-INF\lib
+	   public static final String Path_TestData2 = "C:/Users/Hp/git/20181s_web2/ControleDeEmprestimo/WebContent/WEB-INF/lib/";
+	  
+	   public static final String File_TestData = "roteiros_de_teste.xlsx";
+	   public static final String File_TestData3 = "roteiros_de_teste.xlsx";
+	   public static final String File_TestData2 = "convenioCadastrar.xlsx";
+
+	   public static final String Chrome = "WebContent/WEB-INF/lib/chromedriver.exe";
+	   public static final String Firefox = "WebContent/WEB-INF/lib/geckodriver.exe";
+}
