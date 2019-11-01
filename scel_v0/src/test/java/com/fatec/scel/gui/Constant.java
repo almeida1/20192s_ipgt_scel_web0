9fb8491e79e5b481685057a10fcbe6bd3cf498af
edson <edson@DESKTOP-T4SKGV8>
2019-11-01 13:04:48 -0300

    Ajuste na classe constant e importacao do poi

--- scel_v0/src/test/java/com/fatec/scel/gui/Constant.java
+++ scel_v0/src/test/java/com/fatec/scel/gui/Constant.java
@@ -1,15 +1,15 @@
 package com.fatec.scel.gui;
 
 public class Constant {
-	 public static final String Path_TestData = "C:/temp3/";
+	   //public static final String Path_TestData = "C:/temp3/"; //fatec ipg
 	  // public static final String Path_TestData2 = "E:/edson/2018edson/2018projetodidatico/20181sroteirodetesteguidd/";
 	   //C:\Users\esa\git\20181s_weby\ControleDeEmprestimo\WebContent\WEB-INF\lib
-	   public static final String Path_TestData2 = "C:/Users/Hp/git/20181s_web2/ControleDeEmprestimo/WebContent/WEB-INF/lib/";
+	   public static final String Path_TestData = "F:/20192s_temp/"; //laptop hp
 	  
 	   public static final String File_TestData = "roteiros_de_teste.xlsx";
 	   public static final String File_TestData3 = "roteiros_de_teste.xlsx";
 	   public static final String File_TestData2 = "convenioCadastrar.xlsx";
 
-	   public static final String Chrome = "C:/temp2/chromedriver_win32/chromedriver.exe";
+	   public static final String Chrome = "F:/20192s_temp/chromedriver.exe"; //laptop hp
 	   public static final String Firefox = "WebContent/WEB-INF/lib/geckodriver.exe";
 }
--- scel_v0/src/test/java/com/fatec/scel/gui/ExcelUtils.java
+++ scel_v0/src/test/java/com/fatec/scel/gui/ExcelUtils.java
@@ -3,18 +3,18 @@ package com.fatec.scel.gui;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRichTextString;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
 
-	private static HSSFSheet ExcelWSheet;
-	private static HSSFWorkbook ExcelWBook;
-	private static HSSFCell Cell;
-	private static HSSFRow Row;
+	private static XSSFSheet ExcelWSheet;
+	private static XSSFWorkbook ExcelWBook;
+	private static XSSFCell Cell;
+	private static XSSFRow Row;
 	
 	/**
 	 * este metodo configura o caminho para abrir o arquivo Excel 
@@ -31,7 +31,7 @@ public class ExcelUtils {
 			FileInputStream ExcelFile = new FileInputStream(Path);
 
 			// Acessa a planilha de dados de teste
-			ExcelWBook = new HSSFWorkbook(ExcelFile);
+			ExcelWBook = new XSSFWorkbook(ExcelFile);
 			ExcelWSheet = ExcelWBook.getSheet(SheetName);
          
 		} catch (Exception e) {
