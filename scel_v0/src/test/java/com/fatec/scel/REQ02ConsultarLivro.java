d8297206da4aa7a4d9e4dc9e3ca6abc4867cf955
edson <edson@10.116.16.145>
2019-11-04 23:29:01 -0200

    Refatoraçao do REQ02

--- scel_v0/src/test/java/com/fatec/scel/REQ02ConsultarLivro.java
+++ scel_v0/src/test/java/com/fatec/scel/REQ02ConsultarLivro.java
@@ -2,6 +2,10 @@ package com.fatec.scel;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +21,29 @@ public class REQ02ConsultarLivro {
 	LivroRepository repository ;
 	static Livro livro;  
 	@Test
-	public void test() {
-		// dado que nao existem livros cadastrados
+	public void CT01ConsultaTodos() {
+		// dado que existem livros disponíveis para consulta na biblioteca
 		repository.deleteAll();
-		// quando o usurio inclui as informacoes obrigatorias e confirma a operacao
 		livro = new Livro("3333", "Teste de Software", "Delamaro");
 		repository.save(livro);
+		// quando o usurio solicita consultar todos
+		Iterable<Livro> livros;
+		livros = repository.findAll();
+		//entao o sistema apresenta os livros disponiveis
+		for (Iterator i = livros.iterator(); i.hasNext();) {
+		       Livro livro = (Livro)i.next();
+		}
+		assertThat(livro.getIsbn()).isEqualTo("3333");
+	}
+	@Test
+	public void CT02CosultaPorISBN() {
+		// dado que existem livros disponíveis para consulta na biblioteca
+		repository.deleteAll();
+		livro = new Livro("3333", "Teste de Software", "Delamaro");
+		repository.save(livro);
+		// quando o usurio solicita consultar todos
+		Iterable<Livro> livros;
+		livros = repository.findAll();
 		// entao o sistema valida as informações E envia uma mensagem de aluno cadastrado com sucesso
 		Livro ro = repository.findByIsbn("3333");
 		assertThat(ro).isEqualTo(livro);
